53c91ba8e75ce4a289fdd5c188c8bbdd4e125685
Hannes Diercks <github@xiphe.net>
2016-12-07 19:10:19 +0100

    refactor(convertTime): use moment
    its good

--- src/lib/convertTime.js
+++ src/lib/convertTime.js
@@ -1,26 +1,14 @@
 'use strict';
 
-function leftPad(thing) {
-  if (`${thing}`.length === 1) {
-    return `0${thing}`;
-  }
-
-  return thing;
-}
+const moment = require('moment-timezone');
 
 module.exports = function convertTime(events) {
   return events.map((event) => {
-    const time = new Date(event.start_time);
-    const date = leftPad(time.getDate());
-    const month = leftPad(time.getMonth() + 1);
-    const year = time.getFullYear();
-
-    const dateStr = `${date}.${month}.${year}`;
-    const timeStr = `${leftPad(time.getHours())}:${leftPad(time.getMinutes())}`;
+    const time = moment(event.start_time).tz('MEZ');
 
     return Object.assign({}, event, {
-      dateStr,
-      timeStr,
+      dateStr: time.format('DD.MM.YYYY'),
+      timeStr: time.format('HH:mm'),
     });
   });
 };
