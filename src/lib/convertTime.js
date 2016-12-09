f9f67e5ce8349f93500fbb0938d0fadc8c0eb544
Hannes Diercks <github@xiphe.net>
2016-12-09 13:22:50 +0100

    fix(convertTime): use correct timezone

--- src/lib/convertTime.js
+++ src/lib/convertTime.js
@@ -4,7 +4,7 @@ const moment = require('moment-timezone');
 
 module.exports = function convertTime(events) {
   return events.map((event) => {
-    const time = moment(event.start_time).tz('MEZ');
+    const time = moment(event.start_time).tz('CET');
 
     return Object.assign({}, event, {
       dateStr: time.format('DD.MM.YYYY'),
